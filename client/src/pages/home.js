3bdcd54d0a11f9114caedc0f4b7c4118aee3c0cc
Rafal Galka <rafal.galka@modernweb.pl>
2019-10-21 17:59:37 +0200

    feat(client): vote pin form

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,47 @@
 import m from 'mithril';
 
+export function formGroup(content) {
+  return m('div', { class: 'form-group' }, content);
+}
+
+const pinForm = () =>
+  m(
+    'form',
+    {
+      class: 'pin-form',
+    },
+    [
+      formGroup(
+        m('input', {
+          name: 'voteId',
+          type: 'tel',
+          class: 'form-control',
+          placeholder: 'PIN',
+          id: 'vote-input',
+          value: '',
+        })
+      ),
+      formGroup(
+        m(
+          'button',
+          {
+            class: 'btn btn-block btn-primary',
+            type: 'submit',
+            value: 'Submit',
+          },
+          'Wejdź'
+        )
+      ),
+    ]
+  );
+
 export const Home = {
-  view: () => m('.page', m('h1', 'Hello votely')),
+  view: () =>
+    m(
+      'div',
+      {
+        class: 'd-flex flex-column justify-content-center h-100 w-100 text-center',
+      },
+      m('div', { class: 'container' }, [m('h1', 'Votely'), m('h3', 'Oddaj swój głos!'), pinForm()])
+    ),
 };
